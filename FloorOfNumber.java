36c12d7633f0865d17570e18cc010bf25cc67d39
kiruthika77 <101347717+kiruthika77@users.noreply.github.com>
2022-12-01 14:24:22 +0530

    Add files via upload

---
+++ FloorOfNumber.java
@@ -0,0 +1,38 @@
+package java_practice;
+
+public class FloorOfNumber {
+	public static void main(String []args) {
+		int [] arr= {2,3,4,5,6,7,9,10};
+		int target=8;
+		int ans=(floor(arr,target));
+		
+		System.out.println(ans);
+	}
+	
+	static int floor(int[]arr,int target) {
+		
+		if(arr.length==0) {
+			return -1;
+		}
+		
+		int start=0;int end=arr.length-1;
+		while(start<=end) {
+			int mid=start+((end-start)/2);
+			if(target>arr[mid]) {
+				start=mid+1;
+				
+			}
+			else if(target<arr[mid]) {
+				end=mid-1;
+			}
+			else {
+				return arr[mid];
+			}
+			
+			
+		}
+		
+		return arr[end];
+	}
+
+}
